346b7df20b1b45414de88951bf78abc6d1abdf65
Kirill Borzov <oresh.work@gmail.com>
2016-08-11 19:43:47 +0300

    issue: correct handling with jquery and removing index.js

--- src/imagefill.js
+++ src/imagefill.js
@@ -1,12 +1,25 @@
+/*globals jQuery, define, module, exports, require, window, document, postMessage */
+var imagesLoaded = require('imagesloaded');
+(function (factory) {
+  "use strict";
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  }
+  else if(typeof module !== 'undefined' && module.exports) {
+    module.exports = factory(require('jquery'));
+  }
+  else {
+    factory(jQuery);
+  }
+}(function ($, undefined) {
+  "use strict";
+
 /**
  * imagefill.js
  * Author & copyright (c) 2013: John Polacek
+ * Updated to npm by @oresh
  * johnpolacek.com
  * https://twitter.com/johnpolacek
- *
- * Dual MIT & GPL license
- *
- * Project Page: http://johnpolacek.github.io/imagefill.js
  *
  * The jQuery plugin for making images fill their containers (and be centered)
  *
@@ -20,100 +33,93 @@
  *
  */
 
-var jQuery = require('jquery');
-var imagesLoaded = require('imagesloaded');
-
 // provide jQuery argument
 imagesLoaded.makeJQueryPlugin(jQuery);
 
-;(function($) {
+jQuery.fn.imagefill = function(options) {
+  var $container = this,
+      imageAspect = 1/1,
+      containersH = 0,
+      containersW = 0,
+      defaults = {
+        runOnce: false,
+        target: 'img',
+        throttle : 200  // 5fps
+      },
+      settings = jQuery.extend({}, defaults, options);
 
-  $.fn.imagefill = function(options) {
+  var $img = $container.find(settings.target).addClass('loading').css({'position':'absolute'});
 
-    var $container = this,
-        imageAspect = 1/1,
-        containersH = 0,
-        containersW = 0,
-        defaults = {
-          runOnce: false,
-          target: 'img',
-          throttle : 200  // 5fps
-        },
-        settings = $.extend({}, defaults, options);
+  // make sure container isn't position:static
+  var containerPos = $container.css('position');
+  $container.css({'overflow':'hidden','position':(containerPos === 'static') ? 'relative' : containerPos});
 
-    var $img = $container.find(settings.target).addClass('loading').css({'position':'absolute'});
+  // set containerH, containerW
+  for (var i = 0, len = $container.length; i < len; i++) {
+    var $this = $container.eq(i);
+    containersH += $this.outerHeight();
+    containersW += $this.outerWidth();
+  }
 
-    // make sure container isn't position:static
-    var containerPos = $container.css('position');
-    $container.css({'overflow':'hidden','position':(containerPos === 'static') ? 'relative' : containerPos});
+  // wait for image to load, then fit it inside the container
+  $container.imagesLoaded().done(function(img) {
+    imageAspect = $img.width() / $img.height();
+    $img.removeClass('loading');
+    fitImages();
+    if (!settings.runOnce) {
+      checkSizeChange();
+    }
+  });
 
-    // set containerH, containerW
+  function fitImages() {
+    containersH  = 0;
+    containersW = 0;
     for (var i = 0, len = $container.length; i < len; i++) {
       var $this = $container.eq(i);
-      containersH += $this.outerHeight();
-      containersW += $this.outerWidth();
-    }
+      imageAspect = $this.find(settings.target).width() / $this.find(settings.target).height();
+      var outwrWidth = $this.outerWidth(),
+          outerHeight = $this.outerHeight(),
+          containerW = outwrWidth,
+          containerH = outerHeight,
+          containerAspect = containerW / containerH;
 
-    // wait for image to load, then fit it inside the container
-    $container.imagesLoaded().done(function(img) {
-      imageAspect = $img.width() / $img.height();
-      $img.removeClass('loading');
+      containersH += outerHeight;
+      containersW += outerHeight;
+
+      if (containerAspect < imageAspect) {
+        // taller
+        $this.find(settings.target).css({
+            width: 'auto',
+            height: containerH,
+            top: 0,
+            left: -(containerH * imageAspect - containerW) / 2
+          });
+      } else {
+        // wider
+        $this.find(settings.target).css({
+            width: containerW,
+            height: 'auto',
+            top:-(containerW / imageAspect - containerH) / 2,
+            left: 0
+          });
+      }
+    };
+  }
+
+  function checkSizeChange() {
+    var checkW = 0,
+        checkH = 0;
+    for (var i = 0, len = $container.length; i < len; i++) {
+      var $this = $container.eq(i);
+      checkH += $this.outerHeight();
+      checkW += $this.outerWidth();
+    };
+    if (containersH !== checkH || containersW !== checkW) {
       fitImages();
-      if (!settings.runOnce) {
-        checkSizeChange();
-      }
-    });
-
-    function fitImages() {
-      containersH  = 0;
-      containersW = 0;
-      for (var i = 0, len = $container.length; i < len; i++) {
-        var $this = $container.eq(i);
-        imageAspect = $this.find(settings.target).width() / $this.find(settings.target).height();
-        var outwrWidth = $this.outerWidth(),
-            outerHeight = $this.outerHeight(),
-            containerW = outwrWidth,
-            containerH = outerHeight,
-            containerAspect = containerW / containerH;
-
-        containersH += outerHeight;
-        containersW += outerHeight;
-
-        if (containerAspect < imageAspect) {
-          // taller
-          $this.find(settings.target).css({
-              width: 'auto',
-              height: containerH,
-              top: 0,
-              left: -(containerH * imageAspect - containerW) / 2
-            });
-        } else {
-          // wider
-          $this.find(settings.target).css({
-              width: containerW,
-              height: 'auto',
-              top:-(containerW / imageAspect - containerH) / 2,
-              left: 0
-            });
-        }
-      };
     }
+    setTimeout(checkSizeChange, settings.throttle);
+  }
 
-    function checkSizeChange() {
-      var checkW = 0,
-          checkH = 0;
-      for (var i = 0, len = $container.length; i < len; i++) {
-        var $this = $container.eq(i);
-        checkH += $this.outerHeight();
-        checkW += $this.outerWidth();
-      };
-      if (containersH !== checkH || containersW !== checkW) {
-        fitImages();
-      }
-      setTimeout(checkSizeChange, settings.throttle);
-    }
-    
-    return this;
-  };
+  return this;
+};
 
-}(jQuery));
